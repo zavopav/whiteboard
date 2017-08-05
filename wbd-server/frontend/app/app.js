556bc05421f355e25736cb816ddfd03afc60ffd3
zapplab <pavel.zavodaev@gmail.com>
2017-08-05 20:27:48 +0300

    test

--- wbd-server/frontend/app/app.js
+++ wbd-server/frontend/app/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var wbdApp = angular.module('wbdApp', [
+angular.module('wbdApp', [
   'ngRoute',
   'wbdApp.chat',
   'wbdApp.view2',
   'wbdApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+])
+
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/chat'});
--- wbd-server/frontend/app/chat/chat.js
+++ wbd-server/frontend/app/chat/chat.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('wbdApp.chat', ['ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/chat', {
-            templateUrl: 'chat/chat.html',
-            controller: 'ChatController'
-        });
-    }])
-    .controller('ChatController', ChatController);
 
-function ChatController() {
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/chat', {
+        templateUrl: 'chat/chat.html',
+        controller: 'ChatController'
+    });
+}])
+
+.controller('ChatController', function () {
     console.log('ChatController');
     var socket = new Socket('/ws/chat', function (event) {
         console.log('Received: ' + event.data);
@@ -17,9 +17,9 @@ function ChatController() {
         $('#chat-messages').append($('<li>').text(msg.user + ': ' + msg.text));
     });
 
-    $('#chat-form').submit(function(){
+    $('#chat-form').submit(function () {
         var input = $('#chat-m');
-        socket.send(JSON.stringify({userId: 12, text:input.val()}));
+        socket.send(JSON.stringify({userId: 12, text: input.val()}));
         input.val('');
     });
-}
+});
